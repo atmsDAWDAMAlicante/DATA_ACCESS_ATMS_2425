992ac0e6c0f18c4d701b425c053ed3d28bc21046
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-10-03 20:47:57 +0200

    Working in the Exercise3

--- Unit1/Exercise3/src/Main.java
+++ Unit1/Exercise3/src/Main.java
@@ -1,15 +1,150 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.lang.*;
+import java.util.ArrayList;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Author Velazquez = new Author("Diego de Velazquez", "español");
+        Author Miron = new Author("Miron", "griego");
+        Painting LaVenusDelEspejo = new Painting ("La Venus del Espejo", Painting.typeOfPainting.Oil, "Big");
+        Sculpture Discobolo = new Sculpture("Discóbolo", Sculpture.materials.Marble, Sculpture.styles.Grecoroman);
+        prMessage(LaVenusDelEspejo.getTitle() + " was painted by " + Velazquez.getName() + " Type: " + LaVenusDelEspejo.paintingType);
+        }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+    public static void prMessage(String mss){
+        System.out.printf(mss);
         }
     }
+
+
+// Class Author
+class Author{
+    // Fields - Class Author
+    private String name;
+    private String nationality;
+    //private final ArrayList<Work> workAuthor = new ArrayList();
+    // Constructor - Class Author
+    public Author(String name, String nationality) {
+        this.name = name;
+        this.nationality = nationality;
+    }
+    // Getters and setters - Class Author
+    public String getName() {
+        return name;
+    }
+    public void setName(String value) {
+        name = value;
+    }
+    public String getNationality() {
+        return nationality;
+    }
+    public void setNationality(String value) {
+        nationality = value;
+    }
+
+    // Methods - Class Author
+
+}
+
+// Class Work
+class Work{
+    // Field - Class Work
+    protected String title;
+
+    // Constructor - Class Work
+    public Work(String title) {
+        this.title = title;
+    }
+
+    // Getters and setters - Class Work
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String value){
+        title = value;
+    }
+
+
+    // Methods - Class Room
+
+}
+// Class Painting
+class Painting extends Work{
+
+    enum typeOfPainting {
+        Oil,Pastel, Watercolor
+    }
+
+    // Fields - Class Painting
+    typeOfPainting paintingType;
+    String format;
+    // Constructor - Class Painting
+    public Painting(String title, typeOfPainting paintingType, String format){
+        super(title);
+        this.format = format;
+        this.paintingType = paintingType;
+    }
+
+    // Getters and setters - Class Painting
+    public String getFormat(){
+        return format;
+    }
+    public void setFormat(String value){
+        format = value;
+    }
+
+    // Methods - Class Painting
+
+}
+
+class Sculpture extends Work{
+    enum materials{
+        Bronze, Iron, Marble
+    }
+    enum styles{
+        Neoclassical, Grecoroman, Cubist
+    }
+    // Field - Class Sculpture
+    materials material;
+    styles style;
+
+    // Constructor - Class Sculpture
+    public Sculpture(String title, materials material, styles style){
+        super(title);
+        this.material = material;
+        this.style = style;
+    }
+
+    // Getters and setters - Class Sculpture
+
+
+    // Methods - Class Sculpture
+
+
+}
+
+class Museum{
+    // Field - Class xx
+
+    // Constructor - Class Sculpture
+
+    // Getters and setters - Class Work
+
+    // Methods - Class Room
+
+
+}
+
+class Room{
+    // Field - Class Room
+
+    // Constructor - Class Room
+
+
+    // Getters and setters - Class Room
+
+
+    // Methods - Class Room
 }
